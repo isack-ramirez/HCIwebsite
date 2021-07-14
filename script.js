95eff7c740b4ead0e9280e0c11b6c9da6f6d49ce
isack ramirez <isack.ramirez1@gmail.com>
2021-07-14 15:20:30 -0500

    Added most of website asthetic

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ var number1;
 
 var number2;
 
+
+
+
+var infoCard=`
+<div class="card" style="width: 30rem;">
+<div class="card-body">
+    <h5 class="card-title">Contact us : </h5>
+    <h6 class="card-subtitle mb-2 text-muted">HCI General Contractors</h6>
+    <p class="card-text">Email :  hci.gc.info@gmail.com </p>
+   <p class='card-text'> Phone : (210)223-2500.</p>
+   <a href="mailto:hci.gc.info@gmail.com">Email Us</a>
+ 
+</div>
+</div>
+`
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
@@ -21,6 +37,11 @@ function getRandomInt(max) {
 
   appendEquation();
 
+  function appendInfo(){
+
+
+  }
+
 
 
   function compareAnswer(){
@@ -29,6 +50,7 @@ function getRandomInt(max) {
     if(userInput == (number1 + number2)){
         console.log('same');
         $('#capcha').remove();
+        $('#middleContent').append(infoCard);
 
     }
     else{
